986dbd432826a037977a35747376d3632c044e96
Nicholas Reynolds <sonshine_80@yahoo.com>
2015-11-24 17:36:28 -0800

    Adds a like counter

---
+++ js/main.jsx
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var LikeButton = require('./components/likeButton.jsx');
+var Backbone = require('backbone');
+
+ReactDOM.render(<LikeButton />, document.getElementById('container'));
