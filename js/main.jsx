4015a9aea23ae00e1eb7cfa13b829c1a7ba9ff51
notewrangler <noiseartist@gmail.com>
2015-11-24 23:51:40 -0800

    fallback position

---
+++ js/main.jsx
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Button = require('./components/button.jsx');
+require("../css/main.css");
+
+
+
+ReactDOM.render(<Button />, document.getElementById('container'));
